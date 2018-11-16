812e22fb2c82e2eb18fff94290c499a240d4e87c
aqib feroz <aqibpakii@hotmail.com>
2018-11-16 11:38:24 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './page/navbar/navbar.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+
+const ROUTES=[
+  {path:'dashboard', component:DashboardComponent},
+
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
